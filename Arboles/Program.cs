8bf91fd5ca9fdb41145cc1603deff74faed46a63
jmacasd <63927290+jmacasd@users.noreply.github.com>
2023-06-28 16:10:39 -0700

    Se agrega demostración de árbol con colorización de jerarquía

--- Arboles/Program.cs
+++ Arboles/Program.cs
@@ -12,6 +12,35 @@ namespace Arboles
         {
             ArbolGeneral arbolGeneral = new ArbolGeneral("A");
 
+            Nodo nodoB = arbolGeneral.InsertarNodo("B", arbolGeneral.Raiz);
+            Nodo nodoD = arbolGeneral.InsertarNodo("D", nodoB);
+            Nodo nodoI = arbolGeneral.InsertarNodo("I", nodoD);
+            Nodo nodoE = arbolGeneral.InsertarNodo("E", nodoB);
+            Nodo nodoF = arbolGeneral.InsertarNodo("F", nodoB);
+            Nodo nodoJ = arbolGeneral.InsertarNodo("J", nodoF);
+            Nodo nodoK = arbolGeneral.InsertarNodo("K", nodoF);
+            Nodo nodoC = arbolGeneral.InsertarNodo("C", arbolGeneral.Raiz);
+            Nodo nodoG = arbolGeneral.InsertarNodo("G", nodoC);
+            Nodo nodoH = arbolGeneral.InsertarNodo("H", nodoC);
+            Nodo nodoL = arbolGeneral.InsertarNodo("L", nodoH);
+
+            string arbol = arbolGeneral.ObtenerArbol();
+
+            string[] renglones = arbol.Split('\n');
+
+            foreach (string renglon in renglones)
+            {
+                int contarGuiones = renglon.Count(caracter => caracter.ToString() == "-") + 2;
+                
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), contarGuiones.ToString());
+                
+                Console.WriteLine(renglon);
+                
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Presiona una tecla para salir...");
+            Console.ReadLine();
         }
     }
 }
